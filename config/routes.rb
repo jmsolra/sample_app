16e1bd870725ed1db16ca36173783465bd9daffe
jmsolra <jm.solorzano.rada@gmail.com>
2018-07-22 10:42:00 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
   #get  'static_pages/contact'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup', to: 'users#new'
-  
+  post '/signup',  to: 'users#create'
+  resources :users
 end
