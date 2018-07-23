548ac063feb6c7f605ea1c598e2fd4c8eabda041
jmsolra <jm.solorzano.rada@gmail.com>
2018-07-23 09:41:04 +0000

    Add user microposts2

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,9 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAJI5QY5QXBMEGYR6A'],
-      :aws_secret_access_key => ENV['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']
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  ENV['http://micubos3.us-east-2.amazonaws.com/']
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
